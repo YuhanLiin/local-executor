a5e169344a782abff21e6e208a737ff27016d869
YuhanLiin <yuhanliin+github@protonmail.com>
2025-02-17 00:42:12 -0500

    Put loop into tcp test

--- tests/tcp.rs
+++ tests/tcp.rs
@@ -15,27 +15,31 @@ fn single_thread_echo() {
         let addr = listener.get_ref().local_addr().unwrap();
 
         let fut1 = async {
-            let mut buf = [0u8; 1000];
-            let (mut stream, _) = listener.accept().await.unwrap();
-            log::info!("Task1: Accept");
-            stream.read_exact(&mut buf).await.unwrap();
-            log::info!("Task1: Read");
-            stream.write_all(b"hello world").await.unwrap();
-            log::info!("Task1: Write");
-            assert!(buf.iter().all(|&b| b == 5u8));
+            for i in 1..=10 {
+                let mut buf = [0u8; 1000];
+                let (mut stream, _) = listener.accept().await.unwrap();
+                log::info!("Task1: Accept");
+                stream.read_exact(&mut buf).await.unwrap();
+                log::info!("Task1: Read");
+                stream.write_all(b"hello world").await.unwrap();
+                log::info!("Task1: Write");
+                assert!(buf.iter().all(|&b| b == i));
+            }
         };
 
         let fut2 = async {
-            let mut buf = [0u8; 11];
-            let mut stream = Async::<TcpStream>::connect(addr).await.unwrap();
-            log::info!("Task2: Connect");
-            for _ in 0..10 {
-                stream.write_all(&[5u8; 100]).await.unwrap();
+            for i in 1..=10 {
+                let mut buf = [0u8; 11];
+                let mut stream = Async::<TcpStream>::connect(addr).await.unwrap();
+                log::info!("Task2: Connect");
+                for _ in 0..10 {
+                    stream.write_all(&[i; 100]).await.unwrap();
+                }
+                log::info!("Task2: Write");
+                stream.read(&mut buf).await.unwrap();
+                log::info!("Task2: Read");
+                assert_eq!(&buf, b"hello world");
             }
-            log::info!("Task2: Write");
-            stream.read(&mut buf).await.unwrap();
-            log::info!("Task2: Read");
-            assert_eq!(&buf, b"hello world");
         };
 
         zip(fut1, fut2).await;
